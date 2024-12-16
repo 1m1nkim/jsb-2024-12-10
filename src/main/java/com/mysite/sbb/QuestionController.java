aabdc701b601594928e6860b69239b12dc40e758
1m1nkim <window12ty@naver.com>
2024-12-16 14:30:21 +0900

    question_detail make

--- src/main/java/com/mysite/sbb/QuestionController.java
+++ src/main/java/com/mysite/sbb/QuestionController.java
@@ -2,10 +2,12 @@ package com.mysite.sbb;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -14,7 +16,13 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
+
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list";
+    }
+
     @GetMapping("/sbb")
     @ResponseBody
     public String index(){
@@ -22,9 +30,14 @@ public class QuestionController {
     }
     @GetMapping("/question/list")
     public String list(Model model){
-        List<Question> questionList = questionRepository.findAll();
+        List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         //model로 보낸걸 questionList의 이름으로 타임리프에서 받을 수 있게
         return "question_list";
+    }
+
+    @GetMapping("/question/detail/{id}")
+    public String detial(Model model, @PathVariable("id") Integer id){
+        return "question_detail";
     }
 }
